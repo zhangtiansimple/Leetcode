3f8327c61361c7290ca8b44645f7084964eff425
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-10-09 17:17:57 +0800

    添加 141.环形链表

---
+++ src/com/leetcode/easy/HasCycle.java
@@ -0,0 +1,58 @@
+package com.leetcode.easy;
+
+import com.leetcode.base.ListNode;
+
+/**
+ *
+ * 给定一个链表，判断链表中是否有环。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ * 示例 2：
+ *
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ * 示例 3：
+ *
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ *
+ *
+ * 进阶：
+ *
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ *
+ * */
+public class HasCycle {
+    //快慢指针的应用 如果链表中有环 则快慢指针一定会相遇
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (fast == slow) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
