38d9973addd72c4a79384191ffa3cc64b8fdb87c
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-10-12 15:23:49 +0800

    添加 200.岛屿的数量

---
+++ src/com/leetcode/medium/NumIslands.java
@@ -0,0 +1,74 @@
+package com.leetcode.medium;
+
+/**
+ * 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * <p>
+ * 输出: 1
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * <p>
+ * 输出: 3
+ */
+
+public class NumIslands {
+    //dfs题目 遇到1 遍历所有与其相邻的为1的区域 置为0 即为一个岛屿
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
