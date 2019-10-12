92cf5d92ec44a815351ec8f79965ab4b5fd0ed75
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-10-12 10:41:54 +0800

    添加 695.岛屿的最大面积

---
+++ src/com/leetcode/medium/MaxAreaOfIsland.java
@@ -0,0 +1,63 @@
+package com.leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个包含了一些 0 和 1的非空二维数组 grid , 一个 岛屿 是由四个方向 (水平或垂直) 的 1 (代表土地) 构成的组合。你可以假设二维矩阵的四个边缘都被水包围着。
+ * <p>
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为0。)
+ * <p>
+ * 示例 1:
+ * <p>
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ * [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ * [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ * [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ * [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ * [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * <p>
+ * 对于上面这个给定矩阵应返回 6。注意答案不应该是11，因为岛屿只能包含水平或垂直的四个方向的‘1’。
+ * <p>
+ * 示例 2:
+ * <p>
+ * [[0,0,0,0,0,0,0,0]]
+ * <p>
+ * 对于上面这个给定的矩阵, 返回 0。
+ * <p>
+ * 注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+ */
+
+public class MaxAreaOfIsland {
+    //dfs问题 找到是陆地的点 然后开始向四周搜索 如果是陆地 就更新面积并继续搜索
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(grid, i, j));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+        return  1
+                + dfs(grid, i - 1, j)
+                + dfs(grid, i + 1, j)
+                + dfs(grid, i, j - 1)
+                + dfs(grid, i, j + 1);
+    }
+}
