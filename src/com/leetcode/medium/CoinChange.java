9ae5dfd393f4793a918388f3d52446de016fb255
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-10-14 09:57:51 +0800

    添加 322.零钱兑换

---
+++ src/com/leetcode/medium/CoinChange.java
@@ -0,0 +1,43 @@
+package com.leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ *
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ *
+ * 示例 1:
+ *
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ *
+ * 示例 2:
+ *
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ *
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ * */
+
+public class CoinChange {
+    //背包问题,每个物品都可以无限使用，但是要求背包必须装满，而且要求背包中的物品数目最少
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
