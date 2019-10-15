8c5464e999f2732005fdb73df2844dcf2d9583bd
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-10-15 15:45:06 +0800

    添加 139.单词拆分

---
+++ src/com/leetcode/medium/WordBreak.java
@@ -0,0 +1,52 @@
+package com.leetcode.medium;
+
+import java.util.List;
+/**
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
+ * 说明：
+ *
+ *     拆分时可以重复使用字典中的单词。
+ *     你可以假设字典中没有重复的单词。
+ *
+ * 示例 1：
+ *
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ *
+ * 示例 2：
+ *
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ *      注意你可以重复使用字典中的单词。
+ *
+ * 示例 3：
+ *
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ *
+ *
+ * */
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        //dp[i]表示【0，i - 1】的字符串是否能被wordDict拆分
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                //如果【0， j - 1】能被拆分，并且wordDict里包含【j， i - 1】，那么【0，i - 1】也一定能拆分
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
