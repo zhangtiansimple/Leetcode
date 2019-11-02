daf4d0d9abc89c71d2c738a5f25d80e7f3f7197a
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-11-02 11:04:54 +0800

    添加37.解数独

---
+++ src/com/leetcode/hard/SolveSudoku.java
@@ -0,0 +1,108 @@
+package com.leetcode.hard;
+
+public class SolveSudoku {
+
+    //本题官方解题思路十分精妙 参考官方解题
+    //https://leetcode-cn.com/problems/sudoku-solver/solution/jie-shu-du-by-leetcode/
+
+    // box size
+    int n = 3;
+    // row size
+    int N = n * n;
+
+    int [][] rows = new int[N][N + 1];
+    int [][] columns = new int[N][N + 1];
+    int [][] boxes = new int[N][N + 1];
+
+    char[][] board;
+
+    boolean sudokuSolved = false;
+
+    public boolean couldPlace(int d, int row, int col) {
+    /*
+    Check if one could place a number d in (row, col) cell
+    */
+        int idx = (row / n ) * n + col / n;
+        return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
+    }
+
+    public void placeNumber(int d, int row, int col) {
+    /*
+    Place a number d in (row, col) cell
+    */
+        int idx = (row / n ) * n + col / n;
+
+        rows[row][d]++;
+        columns[col][d]++;
+        boxes[idx][d]++;
+        board[row][col] = (char)(d + '0');
+    }
+
+    public void removeNumber(int d, int row, int col) {
+    /*
+    Remove a number which didn't lead to a solution
+    */
+        int idx = (row / n ) * n + col / n;
+        rows[row][d]--;
+        columns[col][d]--;
+        boxes[idx][d]--;
+        board[row][col] = '.';
+    }
+
+    public void placeNextNumbers(int row, int col) {
+    /*
+    Call backtrack function in recursion
+    to continue to place numbers
+    till the moment we have a solution
+    */
+        // if we're in the last cell
+        // that means we have the solution
+        if ((col == N - 1) && (row == N - 1)) {
+            sudokuSolved = true;
+        }
+        // if not yet
+        else {
+            // if we're in the end of the row
+            // go to the next row
+            if (col == N - 1) backtrack(row + 1, 0);
+                // go to the next column
+            else backtrack(row, col + 1);
+        }
+    }
+
+    public void backtrack(int row, int col) {
+    /*
+    Backtracking
+    */
+        // if the cell is empty
+        if (board[row][col] == '.') {
+            // iterate over all numbers from 1 to 9
+            for (int d = 1; d < 10; d++) {
+                if (couldPlace(d, row, col)) {
+                    placeNumber(d, row, col);
+                    placeNextNumbers(row, col);
+                    // if sudoku is solved, there is no need to backtrack
+                    // since the single unique solution is promised
+                    if (!sudokuSolved) removeNumber(d, row, col);
+                }
+            }
+        }
+        else placeNextNumbers(row, col);
+    }
+
+    public void solveSudoku(char[][] board) {
+        this.board = board;
+
+        // init rows, columns and boxes
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    int d = Character.getNumericValue(num);
+                    placeNumber(d, i, j);
+                }
+            }
+        }
+        backtrack(0, 0);
+    }
+}
