b9c7846a67d7992c7617fbaa0652fbf66783fba4
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-11-09 09:57:49 +0800

    添加 304.二维区域和检索-矩阵不可变

---
+++ src/com/leetcode/medium/NumMatrix.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium;
+
+public class NumMatrix {
+
+    //使用dp数组存储最右角的和 则总和为大矩形减去上矩形减去左矩形在加上小矩形（上矩形和左矩形重复减去）的值
+    private int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
