494f24d2326d731dba9a1e09685252ff2b5f33e6
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2019-11-09 09:12:30 +0800

    添加 402.移掉K位数字

---
+++ src/com/leetcode/medium/RemoveKdigits.java
@@ -0,0 +1,16 @@
+package com.leetcode.medium;
+
+public class RemoveKdigits {
+    //从左到右，找第一个比后面大的字符，删除，清零，k次扫描。
+    public String removeKdigits(String num, int k) {
+        if (num.length() == k) return "0";
+        StringBuilder s = new StringBuilder(num);
+        for (int i = 0; i < k; i++) {
+            int idx = 0;
+            for (int j = 1; j < s.length() && s.charAt(j) >= s.charAt(j - 1); j++) idx = j;
+            s.delete(idx, idx + 1);
+            while (s.length() > 1 && s.charAt(0) == '0') s.delete(0, 1);
+        }
+        return s.toString();
+    }
+}
