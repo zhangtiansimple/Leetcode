b47e857b9ca829a5bb4439208ec30b0fa6ec57e6
zhangtian <530359861@qq.comgit config --global user.name zhangtiansimplessh-keygen -t rsa -C 530359861@qq.com>
2020-01-07 09:46:46 +0800

    添加 1312.让字符串成为回文串的最少插入次数

---
+++ src/com/leetcode/hard/MinInsertions.java
@@ -0,0 +1,81 @@
+package com.leetcode.hard;
+
+/**
+ *
+ * 给你一个字符串 s ，每一次操作你都可以在字符串的任意位置插入任意字符。
+ *
+ * 请你返回让 s 成为回文串的 最少操作次数 。
+ *
+ * 「回文串」是正读和反读都相同的字符串。
+ *
+ * 示例 1：
+ *
+ * 输入：s = "zzazz"
+ * 输出：0
+ * 解释：字符串 "zzazz" 已经是回文串了，所以不需要做任何插入操作。
+ *
+ * 示例 2：
+ *
+ * 输入：s = "mbadm"
+ * 输出：2
+ * 解释：字符串可变为 "mbdadbm" 或者 "mdbabdm" 。
+ *
+ * 示例 3：
+ *
+ * 输入：s = "leetcode"
+ * 输出：5
+ * 解释：插入 5 个字符后字符串变为 "leetcodocteel" 。
+ *
+ * 示例 4：
+ *
+ * 输入：s = "g"
+ * 输出：0
+ *
+ * 示例 5：
+ *
+ * 输入：s = "no"
+ * 输出：1
+ *
+ * 提示：
+ *
+ *     1 <= s.length <= 500
+ *     s 中所有字符都是小写字母。
+ *
+ * */
+
+import java.util.Arrays;
+
+public class MinInsertions {
+
+    //避免重复的计算
+    private int[][] memo;
+    private char[] array;
+
+    public int minInsertions(String s) {
+        array = s.toCharArray();
+        memo = new int[array.length][array.length];
+        for (int[] m : memo) {
+            Arrays.fill(m, -1);
+        }
+        return trackBack(0, array.length - 1);
+    }
+
+    private int trackBack(int start, int end) {
+        if (start >= end) {
+            return 0;
+        }
+
+        if (memo[start][end] != -1) {
+            return memo[start][end];
+        }
+
+        //头尾相等
+        if (array[start] == array[end]) {
+            return trackBack(start + 1, end - 1);
+        }
+
+        // 头尾不相等的最小次数是插入头字符或者插入尾字符的最小值 + 1
+        memo[start][end] = Math.min(trackBack(start + 1, end), trackBack(start, end - 1)) + 1;
+        return memo[start][end];
+    }
+}
