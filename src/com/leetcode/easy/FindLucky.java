40b14202fada0d8732eecc0736b0fd6390f6e450
sky <530359861@qq.com>
2020-03-30 15:16:59 +0800

    添加 1394.找出数组中的幸运数

--- src/com/leetcode/easy/FindLucky.java
+++ src/com/leetcode/easy/FindLucky.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class FindLucky {
     public int findLucky(int[] arr) {
-        int res = 0;
+        int res = -1;
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
